394547638aead41fa07a0e27547dddcd2722a67c
Tsaiwimon <159878347+Tsaiwimon@users.noreply.github.com>
2024-02-13 17:00:50 +0700

    Add files via upload

---
+++ app02.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.sider.text('Sidebar')
+st.sidebar.button('go>')
+st.sidebar.button('went>')
+st.sidebar.button('gone')
+st.title('OOP in ML')
+st.image('https://picsum.photos/500/200')
+st.text_input("what's your name?")
